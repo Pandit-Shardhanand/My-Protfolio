eb77d06c6bf77084b69b929cedb3b7ef66f35464
Shardhanand Pandit <panditshadh@gmail.com>
2024-05-18 17:53:41 +0530

    add some Style and javascript functions

--- js/script.js
+++ js/script.js
@@ -9,9 +9,23 @@ let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop -100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
 
-
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links =>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+    
     // sticky header
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
 
 
     // remove toggle icon and navbar when click navbar links (scroll)
@@ -19,6 +33,7 @@ window.onscroll = () => {
     // animation footer on scroll
 
 }
+
     // Hire me Button 
     document.getElementById("hire").addEventListener("click", function() {
         const recipient = "panditshadh@gmail.com";
@@ -28,26 +43,26 @@ window.onscroll = () => {
         window.location.href = mailtoUrl;
     });
 
-// Contact me 
 
-const form = document.querySwlwctor("form");
-function sendEmail(){
-    Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "panditshadh@gmail.com",
-        Password : "042C5D8DBA9009515A2C970928D3D4024858",
-        To : 'panditshadh@gmail.com',
-        From : "panditshadh@gmail.com",
-        Subject : "This is the subject",
-        Body : "And this is the body"
-    }).then(
-      message => alert(message)
-    );
-}
-form.addEventListener("submit",(e) =>{
-    e.preventDefault();
+// ---------Contact me ----------
+// const form = document.querySwlwctor("form");
+// function sendEmail(){
+//     Email.send({
+//         Host : "smtp.elasticemail.com",
+//         Username : "panditshadh@gmail.com",
+//         Password : "042C5D8DBA9009515A2C970928D3D4024858",
+//         To : 'panditshadh@gmail.com',
+//         From : "panditshadh@gmail.com",
+//         Subject : "This is the subject",
+//         Body : "And this is the body"
+//     }).then(
+//       message => alert(message)
+//     );
+// }
+// form.addEventListener("submit",(e) =>{
+//     e.preventDefault();
     
-    sendEmail();
-})
+//     sendEmail();
+// })
 
 
